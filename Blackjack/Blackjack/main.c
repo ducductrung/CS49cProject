4b1960576c2074eb0a529baab651160257d7befb
jerremy <jferrer408@gmail.com>
2016-11-16 12:24:02 -0800

    initial commit

---
+++ Blackjack/Blackjack/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Blackjack
+//
+//  Created by jerremy ferrer & duc nguyen
+//  CS 49c
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
